c23e3ac83222118ac213e882dfc452d269267e96
Awen Lelu <awen.lelu@conserto.pro>
2022-10-04 16:12:54 +0200

    little cleaning

--- 010-pluginrc.zsh
+++ 010-pluginrc.zsh
@@ -1,5 +1,2 @@
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
-unset ZSH_THEME
 ln -s "$CURR_DIR/spaceship-prompt/spaceship.zsh-theme" "$CURR_DIR/../../xxh-plugin-zsh-ohmyzsh/build/ohmyzsh/themes/spaceship.zsh-theme"
-
-# source $CURR_DIR/spaceship-prompt/spaceship.zsh-theme
